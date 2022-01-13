2ccfed615f6e1ba739ad58c41e7bc5cfea4dc33e
Rahul Singh Solanki <rs.singh1812@gmail.com>
2022-01-14 05:04:52 +0530

    Integrate searchbox on home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,59 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Keyboard, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+
+import {SearchBox} from '../components';
+import {useTagSearch} from '../hooks';
+import {useImageSearch} from '../hooks/useImageSearch';
+import {Size} from '../theme';
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
+  const {state, searchTag} = useTagSearch();
+  const {state: imageSearchState, searchImage} = useImageSearch();
+  const [searchValue, setSearchValue] = useState('funny');
+  const [hideSuggestionList, setHideSuggestionList] = useState(false);
+
+  useEffect(() => {
+    console.log(imageSearchState);
+    console.log(imageSearchState);
+  }, [imageSearchState]);
+
+  const onTagSelect = useCallback(
+    (tag: string) => {
+      setSearchValue(tag);
+      searchTag(tag);
+      setHideSuggestionList(true);
+
+      searchImage(tag);
+
+      Keyboard.dismiss();
+    },
+    [searchImage, searchTag],
+  );
+
+  const onValueChange = useCallback(
+    (value: string) => {
+      if (hideSuggestionList) {
+        setHideSuggestionList(false);
+      }
+      setSearchValue(value);
+      searchTag(value);
+    },
+    [hideSuggestionList, searchTag],
+  );
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Heee asdfdasfad dafadfad </Text>
+      <SearchBox
+        value={searchValue}
+        suggestionLoading={state.isLoading}
+        suggestionsList={state.result}
+        hideSuggestionList={hideSuggestionList}
+        onTagSelect={onTagSelect}
+        onValueChange={onValueChange}
+      />
     </SafeAreaView>
   );
 };
@@ -15,6 +61,8 @@ const Home: React.FC<HomeProps> = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginHorizontal: Size[10],
+    paddingTop: Size[10],
   },
 });
 
