b5aaa7ae09130d92d7581ca2d28b555f4cd37560
Rahul Singh Solanki <rs.singh1812@gmail.com>
2022-01-14 00:40:36 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const App: React.FC<{}> = () => {
+return (
+  <Text>Hello working fine agian</Text>
+)
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
