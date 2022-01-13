6c42bc83193c6407f7bba143d25d28dd006171d9
Rahul Singh Solanki <rs.singh1812@gmail.com>
2022-01-14 05:03:12 +0530

    place AppNavigator type in router folder

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,14 +3,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {Home} from '../screens';
-import {Screens} from '../types/AppNavigator';
+import {Screens} from './AppNavigator';
 
 const Stack = createStackNavigator();
 
 const AppNavigator: FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name={Screens.HOME} component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
