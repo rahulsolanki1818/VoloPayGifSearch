1c70d97fdb35605bed2accbd6ba04889710ce6ff
Rahul Singh Solanki <rs.singh1812@gmail.com>
2022-01-14 05:04:05 +0530

    Add size for auto scalling according to device & colors

---
+++ src/theme/Colors.ts
@@ -0,0 +1,23 @@
+const Colors = {
+  background: {
+    white: '#FFFFFF',
+    transparent: 'transparent',
+    black: '#000000',
+  },
+  inputBox: {
+    background: '#F7F8F8',
+    text: '#ADA4A5',
+  },
+  text: {
+    primary: '#1D1617',
+    secondary: '#FFFFFF',
+    link: '#C58BF2',
+  },
+  border: {
+    primary: '#F7F8F8',
+    gray: 'gray',
+  },
+  overlay: '#00000050',
+};
+
+export {Colors};
